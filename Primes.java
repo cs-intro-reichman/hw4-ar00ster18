af4703534447c2e9876420bffcb2d4f481fdb934
ar00ster18 <arielarusy12@gmail.com>
2024-12-03 18:44:37 +0200

    Maybe it will work like this?

--- Primes.java
+++ Primes.java
@@ -2,15 +2,13 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int primeCounter = 0;
-
-        System.out.println("Prime numbers up to " + n + ":");
-        
         for (int i = 2; i <= n; i++){
             if (isPrime(i)){
                 primeCounter++;
             }
         }
         double primePercent =  ((double) primeCounter/n) * 100;
+        System.out.println("Prime numbers up to " + n + ":");
         System.out.println("There are " + primeCounter + " primes between 2 and " + n + " (" + (int) primePercent + "% are primes)");
     }
 
